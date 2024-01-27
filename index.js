4b1ee1749206313da80d1310d7a6d5c2b1bac380
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-01-26 20:01:12 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(nameData, emailData) {
+    const formData = {
+        name: nameData,
+        email: emailData,
+    }
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const idData = document.createElement("li");
+            idData.textContent = object.id;
+            return document.body.appendChild(idData);
+        })
+        .catch(function (error) {
+            const errorData = document.createElement("li");
+            errorData.textContent = error.message;
+            return document.body.appendChild(errorData);
+        })
+};
